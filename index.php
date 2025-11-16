2fb768f598fbcc28bfdcf028207535ce64fe736b
naye-0 <nasidkayevhenii@gmail.com>
2025-11-16 19:47:19 +0100

    add delete query & fix buttons

--- index.php
+++ index.php
@@ -35,10 +35,8 @@
                     <td><?php echo $row['name'];?></td>
                     <td><?php echo $row['location'];?></td>
                     <td>
-                        <a href="index.php?edit=<?php echo $row['id']; ?>
-                            class=btn btn-info">Edit</a>
-                        <a href="process.php?delete=<?php echo $row['id']; ?>
-                            class=btn btn-danger">Delete</a>
+                        <a href="index.php?edit=<?php echo $row['id']; ?>" class="btn btn-warning">Edit</a>
+                        <a href="process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
                     </td>
                 </tr>
                 <?php endwhile; ?>
--- process.php
+++ process.php
@@ -8,3 +8,8 @@ if (isset($_POST['save'])){
 
     $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
 }
+
+if (isset($_GET['delete'])){
+    $id = intval($_GET['delete']); // Convert to integer for security
+    $mysqli->query("DELETE FROM data WHERE id=$id") or die($mysqli->error);
+}
