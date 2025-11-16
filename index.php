1846a84674bb77f5bca3d3569be904f20f153810
naye-0 <nasidkayevhenii@gmail.com>
2025-11-16 18:50:30 +0100

    init php-crud project

---
+++ index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP CRUD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php
+        require_once 'process.php';
+    ?>
+
+    <?php
+        $mysqli = new mysqli('db', 'root', 'root', 'crud') or die(mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);
+
+        pre_r($result);
+
+        function pre_r( $array ) {
+            echo '<pre>';
+            print_r($array);
+            echo '</pre>';
+        }
+    ?>
+
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="process.php" method="POST">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name:</label>
+                    <input type="text" id="name" name="name" class="form-control" placeholder="Enter your name">
+                </div>
+                <div class="mb-3">
+                    <label for="location" class="form-label">Location:</label>
+                    <input type="text" id="location" name="location" class="form-control" placeholder="Enter your location">
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary" name="save">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </body>
+</html>
